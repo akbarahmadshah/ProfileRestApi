87fdb30ffbebeb77f05d29fd4cf56b793e069536
akbar <akbarahmadshah1@gmail.com>
2020-04-01 20:02:04 +0500

    Added ApiView and PUT, PATCH, DELETE, GET methods

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,11 +1,16 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
+from profiles_api import serializers
 
 
 class HelloApiView(APIView):
     """
     Test ApiView
     """
+    serializer_class = serializers.HelloSerializers
+
     def get(self, request, format=None):
         """
         Returns a list of ApiView features
@@ -20,3 +25,42 @@ class HelloApiView(APIView):
             'kjkljklkljkljkl',
         ]
         return Response({'message': 'Hello', 'an_apiview': an_apiview })
+
+    def post(self, request):
+        """Create a hello message with our name"""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f"Hello {name}"
+            return Response({'message': message})
+        else:
+            return Response(serializer.errors,
+                            status= status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk=None):
+        """
+        Handle update
+        :param request:
+        :param pk:
+        :return:
+        """
+        return Response({'method': 'PUT'})
+
+    def patch(self, request, pk=None):
+        """
+
+        :param request:
+        :param pk:
+        :return:
+        """
+        return Response({'method': 'PATCH'})
+
+    def delete(self, request, pk=None):
+        """
+        Delete an object
+        :param request:
+        :param pk:
+        :return:
+        """
+        return Response({'method': 'DELETE'})
