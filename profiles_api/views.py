240bd770dd83c9d2268b41322a6b1951c83c4611
akbar <akbarahmadshah1@gmail.com>
2020-04-01 17:06:50 +0500

    Hello ApiView

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HelloApiView(APIView):
+    """
+    Test ApiView
+    """
+    def get(self, request, format=None):
+        """
+        Returns a list of ApiView features
+        :param request:
+        :param format:
+        :return:
+        """
+        an_apiview = [
+            'Uses HTTP methds as ..',
+            'jkljkl jkljkljlk',
+            'jkhjkjkjkhjk jhkj',
+            'kjkljklkljkljkl',
+        ]
+        return Response({'message': 'Hello', 'an_apiview': an_apiview })
