5f8686b069ad47385eab3603abc290972865efbe
akbar <akbarahmadshah1@gmail.com>
2020-04-01 23:09:54 +0500

    Added all Viewset Methods

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,7 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-
+from rest_framework import viewsets
 from profiles_api import serializers
 
 
@@ -64,3 +64,77 @@ class HelloApiView(APIView):
         :return:
         """
         return Response({'method': 'DELETE'})
+
+class HelloViewSet(viewsets.ViewSet):
+    """
+    Test API Viewset
+    """
+    serializer_class = serializers.HelloSerializers
+
+    def list(self, request):
+        """
+        Return
+        :param request:
+        :return:
+        """
+        a_viewset = [
+            'badshah kljkljkjlkkl',
+            'badshah kljkljkjlkkl',
+            'badshah kljkljkjlkkl',
+        ]
+
+        return Response({'message': 'Hello!', 'a_viewset': a_viewset})
+
+    def create(self, request):
+        """
+        Create new message
+        :param request:
+        :return:
+        """
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+    def retrieve(self, request, pk=None):
+        """
+
+        :param request:
+        :param pk:
+        :return:
+        """
+        return Response({'http_method': 'GET'})
+
+    def update(self, request, pk=None):
+        """
+
+        :param request:
+        :param pk:
+        :return:
+        """
+        return Response({'http_method': 'PUT'})
+
+    def partial_update(self, request, pk=None):
+        """
+
+        :param request:
+        :param pk:
+        :return:
+        """
+        return Response({'http_method': 'PATCH'})
+
+    def destroy(self, request, pk=None):
+        """
+
+        :param request:
+        :param pk:
+        :return:
+        """
+        return Response({'http_method': 'DELETE'})
