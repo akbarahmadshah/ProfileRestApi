cbe4cf877dcd1782efb3b90200552e6107d5b21c
akbar <akbarahmadshah1@gmail.com>
2020-04-02 13:51:15 +0500

    Permissions added

---
+++ profiles_api/permissions.py
@@ -0,0 +1,20 @@
+from rest_framework import permissions
+
+
+class UpdateOwnProfile(permissions.BasePermission):
+    """
+    Allow user to edit their own profile
+    """
+
+    def has_object_permission(self, request, view, obj):
+        """
+        Check user to edit their own profile only
+        :param request:
+        :param view:
+        :param obj:
+        :return:
+        """
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return obj.id == request.user.id
